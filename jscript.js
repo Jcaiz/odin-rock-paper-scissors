36f7d3b798098affcf2453fa3ae0eba62ff00aea
JuanCaiza <98131956+Jcaiz@users.noreply.github.com>
2024-01-22 19:53:51 -0500

    Created Computer Choice function

--- jscript.js
+++ jscript.js
@@ -1,1 +1,9 @@
 console.log("Hello World")
+
+function getComputerChoice(max) {
+    const Choice = ["Rock", "Paper", "Scissors"]
+    let select = Math.floor(Math.random() * 3);
+    console.log(Choice[select])
+}
+
+getComputerChoice();
