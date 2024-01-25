d19f6bfd3e68e15f2a90bdfcff4264b688e47cd4
JuanCaiza <98131956+Jcaiz@users.noreply.github.com>
2024-01-25 17:19:20 -0500

    Created game loop function for best of 5
    Ends when a player reaches 3 wins, draws don't count as a round

--- jscript.js
+++ jscript.js
@@ -9,7 +9,7 @@ function playRound(compSelection, playerSelection) {
     let pselect = playerSelection[0].toUpperCase() + playerSelection.substr(1).toLowerCase();
     const WIN = `You Win! ${pselect} beats ${compSelection}.`;
     const LOSE = `You Lose! ${compSelection} beats ${pselect}.`;
-    const TIED = `It's a draw!`
+    const TIED = `It's a draw!`;
     console.log(`Player selection was ${playerSelection} and the computer was ${compSelection}.`);
 
     if (pselect == compSelection) {
@@ -19,28 +19,66 @@ function playRound(compSelection, playerSelection) {
 
     if (pselect == "Rock") {
         if (compSelection == "Paper") {
-            return LOSE;
+            // return LOSE;
+            console.log(LOSE)
+            return "L";
         } else {
-            return WIN;
+            // return WIN;
+            console.log(WIN)
+            return "W";
         }
     } else if (pselect == "Paper") {
         if (compSelection == "Scissors") {
-            return LOSE;
+            // return LOSE;
+            console.log(LOSE)
+            return "L";
         } else {
-            return WIN;
+            // return WIN;
+            console.log(WIN)
+            return "W";
         }
     } else if (pselect == "Scissors") {
         if (compSelection == "Rock") {
-            return LOSE;
+            // return LOSE;
+            console.log(LOSE)
+            return "L";
         } else {
-            return WIN;
+            // return WIN;
+            console.log(WIN)
+            return "W";
         }
     } else {
-        return `'${playerSelection}' is an invalid input. Must be either rock, paper, or scissors!`;
+        console.log(`'${playerSelection}' is an invalid input. Must be either rock, paper, or scissors!`);
+        return playRound(getComputerChoice(), prompt("Invalid move. Try again move...").trim());
     }
 }
 
-const compSelection = getComputerChoice();
-let playSelection = prompt("Choose your move...");
+function game(rounds) {
+    let playerWins = 0;
+    let compWins = 0;
+    let wins = rounds - 2;
+    while(true) {
+        let result = playRound(getComputerChoice(), prompt("Choose your move...").trim());
+        if (result == "L") {
+            compWins++;
+            console.log(`Score is Computer ${compWins} and Player ${playerWins}`);
+        } else if (result == "W") {
+            playerWins++;
+            console.log(`Score is Computer ${compWins} and Player ${playerWins}`);
+        }
 
-console.log(playRound(compSelection, playSelection.trim()));
+        if (playerWins == wins) {
+            return "You win!";
+        } else if (compWins == wins){
+            return "Computer wins!"
+        }
+    }
+    // return console.log(WIN);
+}
+
+const BestofRounds = 5;
+// const CompSelection = getComputerChoice();
+// let playSelection = prompt("Choose your move...");
+
+console.log(game(BestofRounds));
+// console.log(playRound(CompSelection, playSelection.trim()));
